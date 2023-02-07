2fa3d26dc4976a94358df8471d58ff0430820d55
kiv1n <aloha@kiv1n.ru>
2023-02-07 16:58:29 +0300

    Add sandbox script

---
+++ scripts/sandbox.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import { Bio__factory } from "../typechain-types";
+
+async function main() {
+  // Init account
+  const accountWallet = new ethers.Wallet(
+    process.env.PRIVATE_KEY_1 || "",
+    ethers.provider
+  );
+
+  // Execute transaction
+  const transaction = await Bio__factory.connect(
+    "0xBfd1b773C4Af9C430883EDC9e70D4b4a4BF8E4d2",
+    accountWallet
+  ).name();
+  console.log("Transaction result:", transaction);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

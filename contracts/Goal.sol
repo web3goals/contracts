bcd085e52a22801f148857a50fd4bcbfb382ba83
kiv1n <aloha@kiv1n.ru>
2023-02-18 00:15:22 +0300

    Add some view functions to goal contract

--- contracts/Goal.sol
+++ contracts/Goal.sol
@@ -348,6 +348,17 @@ contract Goal is
         return _verificationData[tokenId][key];
     }
 
+    function getVerificationDataList(
+        uint256 tokenId,
+        string[] memory keys
+    ) public view returns (string[] memory) {
+        string[] memory values = new string[](keys.length);
+        for (uint i = 0; i < keys.length; i++) {
+            values[i] = (_verificationData[tokenId][keys[i]]);
+        }
+        return values;
+    }
+
     function getWatchers(
         uint256 tokenId
     ) public view returns (DataTypes.GoalWatcher[] memory) {

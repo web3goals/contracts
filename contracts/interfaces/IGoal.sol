995982647a7663726002a1d74a735e16884002e7
kiv1n <aloha@kiv1n.ru>
2023-04-20 18:47:06 +0300

    Delete unnecessary hub and verifier contracts, interfaces, errors

--- contracts/interfaces/IGoal.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.9;
-
-interface IGoal {
-    function getVerificationData(
-        uint256 tokenId,
-        string memory key
-    ) external view returns (string memory);
-}
--- contracts/libraries/Errors.sol
+++ contracts/libraries/Errors.sol
@@ -5,32 +5,21 @@ library Errors {
     // Common errors
     error TokenDoesNotExist();
     error TokenNotTransferable();
-    error ArraysLengthInvalid();
-    error UnableTransfer();
 
     // Goal errors
-    error ProfileAddressNotExists();
     error ProfileNotExists();
     error MessageValueMismatch();
     error StakeInvalid();
     error DeadlineMustBeAtLeast24HoursLater();
-    error VerifierAddressNotExists();
     error GoalClosed();
     error AuthorCannotBeMotivator();
     error AlreadyMotivator();
     error NotAuthor();
     error NotAuthorNotAcceptedMotivator();
-    error NotAchieved();
     error ProofsNotFound();
     error MotivatorNotFound();
     error AlreadyAccepted();
     error SendingStakeToAuthorFailed();
     error SendingStakeToKeeperFailed();
     error SendingStakeToMotivatorFailed();
-
-    // Verifier errors
-    error NotGoalContract();
-    error GoalAlreadyVerified();
-    error AnyProofURINotExists();
-    error GitHubUsernameOrActivityDaysNotExist();
 }
